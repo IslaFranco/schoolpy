369074eb490d8b2788c2a64d8412b7fada94b186
jrobledo <robledo.josej@gmail.com>
2022-10-04 20:20:16 -0600

    added user model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+from django.db import model
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    class Roles(models.TextChoices):
+        TEACHER = "TEACHER", 'Teacher'
+        STUDENT = "STUDENT", 'Student'
+
+    role = models.CharField(max_length=255, choices=Roles.choices)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=14)
+    sex = models.CharField(max_length=1)
+    address = models.CharField(max_length=255)
