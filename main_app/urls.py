d9095dc44cff0558b7792aa9b0540afaec421aa9
islafranco <isla_franco@icloud.com>
2022-10-11 18:43:16 -0700

    added assignments and courses

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,6 +10,17 @@ urlpatterns = [
     path('student-dashboard/', views.studentDashboard, name='student-dashboard'),
     path('accounts/signup/student/', views.student_signup, name='student-signup'),
     path('accounts/signup/teacher/', views.teacher_signup, name='teacher-signup'),
-    path('student/<int:student_id>/',
-         views.student_detail, name='student_detail')
+    path('student/<int:student_id>/', views.student_detail, name='student_detail'),
+    path('assignments/', views.assignments_index, name='assignment_index'),
+    path('assignments/<int:assignment_id>/', views.assignments_detail, name='assignment_detail'),
+    path('assignments/create/', views.AssignmentCreate.as_view(), name='assignment_create'),
+    path('assignments/<int:pk>/update/', views.AssignmentUpdate.as_view(), name='assignment_update'),
+    path('assignments/<int:pk>/delete/', views.AssignmentDelete.as_view(), name='assignment_delete'),
+    path('courses/', views.courses_index, name='course_index'),
+    path('courses/<int:course_id>/', views.courses_detail, name='course_detail'),
+    path('courses/create/', views.CourseCreate.as_view(), name='course_create'),
+    path('courses/<int:pk>/update/', views.CourseUpdate.as_view(), name='course_update'),
+    path('courses/<int:pk>/delete/', views.CourseDelete.as_view(), name='course_delete'),
+
+
 ]
