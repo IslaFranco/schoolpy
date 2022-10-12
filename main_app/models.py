51ad40bb2a9d381a347e27efb46257a1b21757d5
jrobledo <robledo.josej@gmail.com>
2022-10-11 20:33:47 -0600

    added courses as many to many field in Student model

--- main_app/models.py
+++ main_app/models.py
@@ -74,6 +74,7 @@ class Student(User):
     grade = models.CharField(max_length=100)
     dob = models.DateField()
 
+    teachers = models.ManyToManyField(Teacher)
     courses = models.ManyToManyField(Course)
     assignments = models.ManyToManyField(Assignment)
 
