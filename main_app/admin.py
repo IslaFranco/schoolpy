d6ac761ce79c74a489faeece897e105a05fb6eab
Jesse Robledo <robledo.josej@gmail.com>
2022-10-14 09:02:57 -0600

    added courses taught and taken through tables to admin panel

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Student, Teacher, Assignment, Course
+from .models import Student, Teacher, Assignment, Course, CourseTaught, CourseTaken
 
 # Register your models here.
 admin.site.register(Student)
 admin.site.register(Teacher)
 admin.site.register(Assignment)
 admin.site.register(Course)
+admin.site.register(CourseTaught)
+admin.site.register(CourseTaken)
