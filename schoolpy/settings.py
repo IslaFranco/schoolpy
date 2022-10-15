bdd326810509a83b436c3d02f8a237e2eb7d5d4c
islafranco <isla_franco@icloud.com>
2022-10-14 20:42:48 -0700

    deployment

--- schoolpy/settings.py
+++ schoolpy/settings.py
@@ -16,7 +16,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-import environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
