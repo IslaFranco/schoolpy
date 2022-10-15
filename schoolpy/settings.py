c5427d0935d44e1f9bcca4cf6f1064e7ea904c69
islafranco <isla_franco@icloud.com>
2022-10-14 20:46:39 -0700

    deployment

--- schoolpy/settings.py
+++ schoolpy/settings.py
@@ -15,6 +15,11 @@ import environ
 
 import os
 
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
