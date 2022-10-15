71b33e1c77b8dfe230ae342f980111deac3530bc
islafranco <isla_franco@icloud.com>
2022-10-14 20:41:08 -0700

    deployment

--- schoolpy/settings.py
+++ schoolpy/settings.py
@@ -13,10 +13,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 
 import os
-import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+import environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
