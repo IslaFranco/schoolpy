c7bd77ffdf7f70217bf169be1c6c92810cf34fa0
islafranco <isla_franco@icloud.com>
2022-10-14 20:27:07 -0700

    deployment #2

--- schoolpy/settings.py
+++ schoolpy/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_NAME'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': 'db',
+        # 'HOST': 'db',
     }
 }
 
