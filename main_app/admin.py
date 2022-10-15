9105eb1fdfb27320006bb3488e0915803650ed6d
Jesse Robledo <robledo.josej@gmail.com>
2022-10-14 21:27:19 -0600

    imported assignment models

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Student, Teacher, Assignment, Course, CourseTaught, CourseTaken
+from .models import Student, Teacher, Assignment, Course, CourseTaught, CourseTaken, StudentAssignment, TeacherAssignment, CourseAssignment
 
 # Register your models here.
 admin.site.register(Student)
