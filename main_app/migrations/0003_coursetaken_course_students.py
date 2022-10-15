8ea217ea30ed5019e72dfd335552596259c09bae
Jesse Robledo <robledo.josej@gmail.com>
2022-10-14 20:35:48 -0600

    removed bad ref to nonexistant migration

--- main_app/migrations/0003_coursetaken_course_students.py
+++ main_app/migrations/0003_coursetaken_course_students.py
@@ -7,7 +7,6 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('auth', '0013_student_teacher'),
         ('main_app', '0002_coursetaught_course_teachers'),
     ]
 
